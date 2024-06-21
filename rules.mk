fcacf7ddb2a2ce2af04f6a67dc7adcae066653d1
Qing Wu <wiserfirst@gmail.com>
2024-06-21 11:12:48 +1000

    update keymap and gen ascii visualization

--- rules.mk
+++ rules.mk
@@ -1,7 +1,18 @@
+# Automatically update the keymap visualization when compiling
+# The '-' in front of keymapviz is used to ignore errors spewed by keymapviz.
+KEYMAP_FOLDER = keyboards/ergodox_ez/keymaps/iris
+.PHONY: %
+%:
+	-keymapviz -k ergodox_ez -l plain -r $(KEYMAP_FOLDER)/keymap.c -c $(KEYMAP_FOLDER)/visualisation/keymapviz.ini > /dev/null
+
 MOUSEKEY_ENABLE = yes
 LTO_ENABLE = yes
 COMMAND_ENABLE = no
+KEY_LOCK_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
 WEBUSB_ENABLE = yes
-ORYX_ENABLE = yes
+ORYX_ENABLE = no
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
